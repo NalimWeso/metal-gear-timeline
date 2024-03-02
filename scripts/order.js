8d296fa48893a32a0422e28ed2cb39911b4e49b1
Milan Wesołowski <milanweso@gmail.com>
2024-03-02 19:56:48 +0100

    Update order.js

--- scripts/order.js
+++ scripts/order.js
@@ -1,5 +1,4 @@
 const pageStart = document.querySelector('.page-start');
-const pageStart2 = document.querySelector('.page-start');
 const content = document.querySelector('.content');
 
 pageStart.innerHTML = title + typesExplanation + filterOptions;
